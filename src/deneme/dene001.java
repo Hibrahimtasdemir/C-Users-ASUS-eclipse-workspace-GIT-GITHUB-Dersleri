9c75b29109fc3b33a1ad3d4c98b672198b6d151f
Hibrahimtasdemir <h.ibrahimtasdemir200@gmail.com>
2022-06-03 22:51:16 +0200

    v002

--- src/deneme/dene001.java
+++ src/deneme/dene001.java
@@ -1,5 +1,17 @@
 package deneme;
 
+import java.util.Scanner;
+
 public class dene001 {
 
+	public static void main(String[] args) {
+		// 1 satýr
+		// 2 satýr
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = scan.nextLine();
+		
+		
+	}
 }
